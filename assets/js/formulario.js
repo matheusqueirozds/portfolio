3427881a345e91d94641b735d733607ac47297b5
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-08-18 23:48:13 -0300

    Adicionei o js na mensagem de envio do formulário

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -1,44 +1,47 @@
-const formulario = document.querySelector("form");
-const contatoMensagem = document.querySelector(".contato-mensagem");
-const mensagemEnviada = document.querySelector(".contato .mensagem-enviada");
-const mensagemErro = document.querySelector(".contato .mensagem-erro");
-const botoes = document.querySelectorAll(
-    ".mensagem-enviada .botao, .mensagem-erro .botao"
-);
+function initMostrarMensagem() {
+    const formulario = document.querySelector(".js form");
+    const contatoMensagem = document.querySelector(".js .js-contato-mensagem");
+    const mensagemEnviada = document.querySelector(".js .mensagem-enviada");
+    const mensagemErro = document.querySelector(".js .mensagem-erro");
+    const botoes = document.querySelectorAll(
+        ".js .mensagem-enviada .botao, .js .mensagem-erro .botao"
+    );
 
-botoes.forEach((botao) => {
-    function handleBotao(e) {
-        if (botao.classList.contains("botao")) {
-            location.reload();
+    botoes.forEach((botao) => {
+        function handleBotao(e) {
+            if (botao.classList.contains("botao")) {
+                location.reload();
+            }
+        }
+        botao.addEventListener("click", handleBotao);
+    });
+
+    function formularioEnviado(resposta) {
+        if (resposta.ok) {
+            contatoMensagem.classList.add(activeClass);
+            mensagemEnviada.classList.add(activeClass);
+            formulario.classList.add("inativo");
+        } else {
+            contatoMensagem.classList.add(activeClass);
+            mensagemErro.classList.add(activeClass);
+            formulario.classList.add("inativo");
         }
     }
-    botao.addEventListener("click", handleBotao);
-});
 
-function formularioEnviado(resposta) {
-    if (resposta.ok) {
-        contatoMensagem.classList.add("ativo");
-        mensagemEnviada.classList.add("ativo");
-        formulario.classList.add("inativo");
-    } else {
-        contatoMensagem.classList.add("ativo");
-        mensagemErro.classList.add("ativo");
-        formulario.classList.add("inativo");
+    function enviarFormulario(e) {
+        e.preventDefault();
+        const botao = document.querySelector("form button");
+
+        botao.disabled = true;
+        botao.innerText = "Enviando...";
+
+        const data = new FormData(formulario);
+
+        fetch("/enviar.php", {
+            method: "POST",
+            body: data,
+        }).then(formularioEnviado);
     }
+    formulario.addEventListener("submit", enviarFormulario);
 }
-
-function enviarFormulario(e) {
-    e.preventDefault();
-    const botao = document.querySelector("form button");
-
-    botao.disabled = true;
-    botao.innerText = "Enviando...";
-
-    const data = new FormData(formulario);
-
-    fetch("/enviar.php", {
-        method: "POST",
-        body: data,
-    }).then(formularioEnviado);
-}
-formulario.addEventListener("submit", enviarFormulario);
+initMostrarMensagem();
